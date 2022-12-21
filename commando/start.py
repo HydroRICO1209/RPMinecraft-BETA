e5f2e1aefc791a43d9270849419ad4839ffa31d3
HydroRICO <juanhanng1209@gmail.com>
2022-12-21 16:28:15 +0800

    Update start.py

--- commando/start.py
+++ commando/start.py
@@ -21,8 +21,8 @@ class Start(commands.Cog):
 INSERT INTO armors (playerid, helmet, chestplate, leggings, boots, sword)
 VALUES ($1, 0, 0, 0, 0, 0)
 ''', userid)
-            except:
-                raise error
+            except Exception as e:
+                print(e)
                 await ctx.send('Your account already exist')
                 pass
             
@@ -32,7 +32,8 @@ VALUES ($1, 0, 0, 0, 0, 0)
 INSERT INTO farm (playerid, small_sapling, medium_sapling, large_sapling, apple, wheat_seeds, wheat, potato, poisonous_potato, carrot, beetroot_seeds, beetroot, cleansed_water_bucket. cleansed_dirt)
 VALUES ($1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 ''', userid)
-            except:
+            except Exception as e:
+                print(e)
                 await ctx.send('Your account already exist')
                 pass
                 
@@ -42,7 +43,8 @@ VALUES ($1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 INSERT INTO farmlist (playerid, slot1, slot1time, slot2, slot2time, slot3, slot3time)
 VALUES ($1, 'locked', 999999, 'locked', 999999, 'locked', 999999)
 ''', userid)
-            except:
+            except Exception as e:
+                print(e)
                 await ctx.send('Your account already exist')
                 pass
 
@@ -52,7 +54,8 @@ VALUES ($1, 'locked', 999999, 'locked', 999999, 'locked', 999999)
 INSERT INTO misc (playerid, emerald, cobble, coal, iron_ingot, diamond, gold_ingot, netherite_scrap, netherite_ingot, redstone, soul_sand, wood, bed, common_chest, rare_chest	super_rare_chest, epic_chest, mythic_chest, legendary_chest)
 VALUES ($1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 ''', userid)
-            except:
+            except Exception as e:
+                print(e)
                 await ctx.send('Your account already exist')
                 pass
             
@@ -62,7 +65,8 @@ VALUES ($1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 INSERT INTO mobdrop (playerid, pogchop, cooked_pogchop, beef, steak, wool, map_scrap, map, wither_skull, blaze_rod, blaze_powder, ender_pearl, eye_of_ender)
 VALUES ($1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 ''', userid)
-            except:
+            except Exception as e:
+                print(e)
                 await ctx.send('Your account already exist')
                 pass
             
@@ -72,7 +76,8 @@ VALUES ($1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 INSERT INTO stats (playerid, hp, level, highest_area, atk, defend, xp, area, vote_count)
 VALUES ($1, 100, 1, 1, 1, 1, 1, 0, 1, 0)
 ''', userid)
-            except:
+            except Exception as e:
+                print(e)
                 await ctx.send('Your account already exist')
                 pass
             
