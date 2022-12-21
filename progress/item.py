4bbd23291d0ce50226668e01029863d4420184ae
HydroRICO <juanhanng1209@gmail.com>
2022-12-21 15:34:49 +0800

    Update item.py

--- progress/item.py
+++ progress/item.py
@@ -1,6 +1,2 @@
-class Item():
-    def __init__(self, ctx):  
-        self.userid = str(ctx.author.id)
-    
-    async def fetchvalue(self, item, tablename):
-        return (await self.bot.db.fetch('SELECT $1 FROM $2 WHERE userid = $3', item, tablename, self.userid))[0][item]
+async def fetchvalue(self, item, tablename):
+    return (await self.bot.db.fetch('SELECT $1 FROM $2 WHERE userid = $3', item, tablename, self.userid))[0][item]
