6ac230055c147e45decabdffc088cacc3012a625
HydroRICO <juanhanng1209@gmail.com>
2022-12-21 15:27:19 +0800

    Update test.py

--- commando/test.py
+++ commando/test.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import discord
-from progress.item.Item import fetchvalue
+from progress.item import fetchvalue
 
 class Test(commands.Cog):
     def __init__(self, bot):
