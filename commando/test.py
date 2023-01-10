641eed5010a4649dce8670a59eac4a3cd39085cf
HydroRICO <juanhanng1209@gmail.com>
2023-01-10 16:42:02 +0800

    Update test.py

--- commando/test.py
+++ commando/test.py
@@ -9,12 +9,12 @@ class Test(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def test(self, ctx, *, arg):
-        dbfunc = progress.database.Database(bot)
+        dbfunc = self.bot.database_handler
         arglists = arg.split(" ")
         arglen = len(arglists)
         if arglen == 3:
             #item, tablename, userid
-            var = await dbfunc.fetchvalue(arglists[0], arglists[1], int(arglists[2]))
+            var = await .fetchvalue(arglists[0], arglists[1], int(arglists[2]))
             print(var)
         else:
             await ctx.send(f'arglen is only {arglen}, it should be 3 dumb')
