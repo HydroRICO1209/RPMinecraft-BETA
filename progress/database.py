e997b98aad441269fe7a42eeb7443f5ece637fb3
HydroRICO <juanhanng1209@gmail.com>
2023-01-12 16:33:07 +0800

    Update database.py

--- progress/database.py
+++ progress/database.py
@@ -5,14 +5,18 @@ class Database:
     def __init__(self, bot):
         self.bot = bot
 
-    async def fetchvalue(self, item, tablename, userid):
-        query = f'SELECT {item} FROM {tablename} WHERE playerid = $1'
-        return (await self.bot.db.fetchval(query, int(userid)))
+    async def fetchValue(self, item, tablename, userid):
+        fetch_query = f'SELECT {item} FROM {tablename} WHERE playerid = $1'
+        return (await self.bot.db.fetchval(fetch_query, int(userid)))
     
-    async def updatevalue(self, item, tablename, userid, newvalue):
-        query = f'UPDATE {tablename} SET {item} = $1 WHERE playerid = $2'
-        await self.bot.db.execute(query, int(newvalue), int(userid))
-
-#async def changevalue(item, tablename, userid, changes):
-#   value = (await bot.db.fetch('SELECT $1 FROM $2 WHERE userid = $3', item, tablename, userid))[0][item]
-#  value += changes
+    async def updateIntValue(self, item, tablename, userid, number):
+        fetch_query = f'SELECT {item} FROM {tablename} WHERE playerid = $1'
+        value = await self.bot.db.fetchval(fetch_query, int(userid))
+        newvalue = int(number) + value
+        
+        update_query = f'UPDATE {tablename} SET {item} = $1 WHERE playerid = $2'
+        await self.bot.db.execute(update_query, int(newvalue), int(userid))
+        
+    async def updateBoolValue(self, item, tablename, userid, Truth):
+        update_query = f'UPDATE {tablename} SET {item} = $1 WHERE playerid = $2'
+        await self.bot.db.execute(update_query, bool(Truth), int(userid))
