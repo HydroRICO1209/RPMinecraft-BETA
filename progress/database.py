5fe8e2d1ce80b311e810697ca2b2873c95a6ac5b
HydroRICO <juanhanng1209@gmail.com>
2023-01-12 16:10:55 +0800

    Update database.py

--- progress/database.py
+++ progress/database.py
@@ -6,12 +6,12 @@ class Database:
         self.bot = bot
 
     async def fetchvalue(self, item, tablename, userid):
-        string1 = f'SELECT {item} FROM {tablename} WHERE playerid = $1'
-        return (await self.bot.db.fetchval(string1, int(userid)))
+        query = f'SELECT {item} FROM {tablename} WHERE playerid = $1'
+        return (await self.bot.db.fetchval(query, int(userid)))
     
     async def updatevalue(self, item, tablename, userid, newvalue):
-        strin1 = f'UPDATE {tablename} SET {item} = $1 WHERE playerid = $2'
-        await self.bot.db.execute(string1, int(newvalue), int(userid))
+        query = f'UPDATE {tablename} SET {item} = $1 WHERE playerid = $2'
+        await self.bot.db.execute(query, int(newvalue), int(userid))
 
 #async def changevalue(item, tablename, userid, changes):
 #   value = (await bot.db.fetch('SELECT $1 FROM $2 WHERE userid = $3', item, tablename, userid))[0][item]
