70287ff2cea39344137839faa9e99bb7372de910
HydroRICO <juanhanng1209@gmail.com>
2023-01-14 14:54:08 +0800

    asd

--- commando/area.py
+++ commando/area.py
@@ -11,19 +11,21 @@ class Area(commands.Cog):
     async def area(self, ctx, arg: int):
         try:
             dbfunc = self.bot.database_handler
+            username = ctx.author.name
             userid = ctx.author.id
             area = await dbfunc.fetchValue('area', 'stats', userid)
             highestArea = await dbfunc.fetchValue('highest_area', 'stats', userid)
+            
             if 1 <= arg <= 14:
                 if arg <= highestArea:
                     await dbfunc.setIntValue('area', 'stats', userid, arg)
-                    await ctx.send(f'**{ctx.author.name}** has moved to **AREA {arg}**')
+                    await ctx.send(f'**{username}** has moved to **AREA {arg}**')
                 else:
-                    await ctx.send(f'**{ctx.author.name}** fight more bosses to unlock this area :D')
+                    await ctx.send(f'**{username}** fight more bosses to unlock this area :D')
             else:
                 await ctx.send('404 area not found')
         except KeyError: #error handler
-            await ctx.send(f'**{ctx.author.name}**, your account is either not created yet or not at the latest version. Try using `rpm start`')
+            await ctx.send(f'**{username}**, your account is either not created yet or not at the latest version. Try using `rpm start`')
 
 async def setup(bot):
     await bot.add_cog(Area(bot))
--- commando/boss.py
+++ commando/boss.py
@@ -11,81 +11,93 @@ class Boss(commands.Cog):
     @commands.cooldown(1, 86400, commands.BucketType.user)
     async def boss(self, ctx):
         try:
-            stats = Stats(ctx)
-
-            if stats.highestArea > stats.area:
+            dbfunc = self.bot.database_handler
+            userid = ctx.author.id
+            username = ctx.author.name
+            highestArea = await dbfunc.fetchValue('highest_area', 'stats', userid)
+            area = await dbfunc.fetchValue('area', 'stats', userid)
+            atk = await dbfunc.fetchValue('atk', 'stats', userid)
+            defend = await dbfunc.fetchValue('defend', 'stats', userid)
+            hp = await dbfunc.fetchValue('hp', 'stats', userid)
+            xp = await dbfunc.fetchValue('xp', 'stats', userid)
+            level = await dbfunc.fetchValue('level', 'stats', userid)
+            
+            if higherArea > area:
                 await ctx.send('This area doesnt have any boss anymore')
-            elif stats.highestArea == stats.area:
-                if stats.area == 1:
+            elif highestArea == area:
+                if area == 1:
                     bossName = 'The Mutated Poggy'
                     mobdef = 50
                     mobatk = 15
                     newArea = 2
-                elif stats.area == 2:
+                elif area == 2:
                     bossName = 'Giant Zompiggy'
                     mobdef = 100
                     mobatk = 25
                     newArea = 3
-                elif stats.area == 3:
+                elif area == 3:
                     bossName = 'woc eth , enobrennid'
                     mobdef = 200
                     mobatk = 35
                     newArea = 4
-                elif stats.area == 4:
+                elif area == 4:
                     bossName = 'GIANT SPIDER'
                     mobdef = 500
                     mobatk = 45
                     newArea = 5
-                elif stats.area == 5:
+                elif area == 5:
                     bossName = 'Wooly Skeleton'
                     mobdef = 800
                     mobatk = 56
                     newArea = 6
-                elif stats.area == 6:
+                elif area == 6:
                     bossName = 'Skeleton KING'
                     mobdef = 1000
                     mobatk = 70
                     newArea = 7
-                elif stats.area == 7:
+                elif area == 7:
                     bossName = 'Johnny, THE RAIDER'
                     mobdef = 1500
                     mobatk = 135
                     newArea = 8
-                elif stats.area == 8:
+                elif area == 8:
                     bossName = 'Elder Guardian'
                     mobdef = 2000
                     mobatk = 300
                     newArea = 8
-                elif stats.area == 9:
+                elif area == 9:
                     bossName = 'Cheezy Blaze'
                     mobdef = 2000
                     mobatk = 145
                     newArea = 10
-                elif stats.area == 10:
+                elif area == 10:
                     bossName = 'GIANT MAGMA CUBE'
                     mobdef = 2300
                     mobatk = 175
                     newArea = 11
-                elif stats.area == 11:
+                elif area == 11:
                     bossName = 'small wither'
                     mobdef = 2500
                     mobatk = 200
                     newArea = 12
-                elif stats.area == 13:
+                elif area == 13:
                     bossName = 'Silvered Enderman'
                     mobdef = 2800
                     mobatk = 380
                     newArea = 14
                 
-                mobatktotal = round(mobdef / stats.atk)
-                damage = (mobatk - stats.defend) * mobatktotal
+                mobatktotal = round(mobdef / atk)
+                damage = (mobatk - defend) * mobatktotal
                 if damage < 0: damage = 0
                 newhp = stats.hp - damage
                 
                 if newhp > 0:
-                    db[stats.userid + 'hp'] = newhp
-                    db[stats.userid + 'highestArea'] = newArea
-                    db[stats.userid + 'area'] = newArea
+                    await dbfunc.setIntValue('hp', 'stats', userid, newhp)
+                    await dbfunc.setIntValue('highest_area', 'stats', userid, newArea)
+                    await dbfunc.setIntValue('area', 'stats', userid, newArea)
+                    db[userid + 'hp'] = newhp
+                    db[userid + 'highestArea'] = newArea
+                    db[userid + 'area'] = newArea
                     await ctx.send(f'''
     **{ctx.author.name}** went into a BIGGG cave to find the boss
     Looks like **{ctx.author.name}** found **{bossName}**
